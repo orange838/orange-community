f0ea108a6c256aee1c3c7cc02c9653f77538f47d
orange838 <3659793158@qq.com>
2026-08-30 17:17:06 +0800

    修正导入

--- orange-frontend/src/router/index.js
+++ orange-frontend/src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 // 1. 引入你左侧目录里建好的那 4 个文件
 // 注意路径：因为 index.js 在 router 文件夹里，所以要退一级到 view 文件夹
 import Home from '../view/Home.vue'
-import Checkin from '../view/Checkin.vue'
+import Checkin from '../view/CheckIn.vue'
 import Notice from '../view/Notice.vue'
 import Profile from '../view/Profile.vue'
 
